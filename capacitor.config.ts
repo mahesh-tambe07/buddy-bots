c54ab00d15c863e59963f96b7d0bae70f01b3dc8
Mahesh Tambe <maheshtambe5112@gmail.com>
2025-12-27 23:16:44 +0530

    update file

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.buddybots.app',
-  appName: 'buddybots',
-  webDir: 'build'
-};
-
-export default config;
